a7d561552c72cda936258c222e997f24d37a7968
dev-LuisSM <dev.luissm@gmail.com>
2022-06-17 23:02:15 -0500

    Agregando API Giphy y usando hooks (State y Effect)

--- 02-giphy/src/App.js
+++ 02-giphy/src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const GIFS = [
-  "https://media3.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.gif?cid=ecf05e47ryil0409i06oxn1qojh9mrj799t981oi3g2bs3jb&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/ialSDndyZsksMRJWsb/giphy.gif?cid=790b7611a5e683541ea979d987ae8fd2716974fe8e4ce9c3&rid=giphy.gif&ct=g",
-];
-
-const DIFF = [
-  "https://media3.giphy.com/media/xNuoUMEJCdVKVm7r2x/giphy.gif?cid=ecf05e47j1zatp4hsujnbo5742j60o125cak7cmi7ij15tow&rid=giphy.gif&ct=g",
-  "https://media0.giphy.com/media/otJXCilaZR8IuAFURV/giphy.gif?cid=ecf05e47j9wxoqevpt1eeahbk3azzillye6mp2o77708h6wa&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/QDf24rINdjHwD3vOLV/giphy.gif?cid=ecf05e47y7pkvigch28ynsupcb667nidydkrhlx34r7y7v7s&rid=giphy.gif&ct=g",
-];
+const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=Fvh4VzUmoBQy7FN3F71VDNHqdXBKd9Pp&q=panda&limit=10&offset=0&rating=g&lang=en'
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS);
+  const [gifs, setGifs] = useState([]);
+  
+  useEffect(() => {
+    console.log("Efecto ejecutado");
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(respuesta => {
+      const {data} = respuesta
+      const gifs = data.map(image => image.images.downsized_medium.url)
+      setGifs(gifs)
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -21,7 +23,6 @@ function App() {
         {gifs.map((singleGif) => (
           <img src={singleGif} alt="Gif test" />
         ))}
-        <button onClick={() => setGifs(DIFF)}> Cambiar gifs </button>
       </section>
     </div>
   );
